d8d7696f33428864c03021969b1f765b233df099
Abhishek Patil <abishkpatil@gmail.com>
2024-05-22 19:49:31 +0530

    feat: add basic home structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/pages/Home"
 
 function App() {
-  return <div>
-    Hi
-  </div>
+  return <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#212121", //main
+    },
+    secondary: {
+      main: "#171717", //sidebar
+    },
+    tertiary:{
+      main:"#2F2F2F" //text input
+    },
+    lightWhite:{
+      main:"#ECECEC"
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
